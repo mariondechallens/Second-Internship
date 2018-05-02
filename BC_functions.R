40cec56a1f2dc5bca08ff4c2947e8e96a9c49bb7
mariondechallens <35600270+mariondechallens@users.noreply.github.com>
2018-05-02 10:20:15 +0200

    Add files via upload

--- BC_functions.R
+++ BC_functions.R
@@ -1,4 +1,4 @@
-####Fonctions utiles pour estimer la p-valeur 
+####Fonctions utiles pour estimer la p-valeur
 
 #calcul du nombre de stats simulees superieures a Zobs
 nb_exc<-function(x0,z){  ##pas besoin d'une boucle en fait
@@ -23,7 +23,7 @@ k_func <- function(u, z) -mean(log(1-u*z))
 
 Lp <- function(u,z) {
   n <- length(z)
-  if(u == 0) 
+  if(u == 0)
     return( -n*(log(mean(z)) + 1) )
   k <- k_func(u,z)
   n*(log(u/k) + k - 1)
@@ -31,7 +31,7 @@ Lp <- function(u,z) {
 
 EMV <- function(z) {
   u <- optimize(function(u) Lp(u,z), c(-10, min(1/z)), maximum=TRUE )$maximum
-  if(u == 0) 
+  if(u == 0)
     return(list( a = mean(z), k = 0))
   k <- k_func(u, z)
   list(a = k/u, k = k)
@@ -47,18 +47,28 @@ FGPD<-function(z,zexc,estim){
   a<-coeff$a
   k<-coeff$k
   if (k!= 0)
-    return(1-(1-k*z/a)^(1/k))
+    return(list(val = 1-(1-k*z/a)^(1/k),
+                k = k,
+                a = a))
   else
-    return(1-exp(-z/a))
+    return(list(val = 1-exp(-z/a),
+                k = k,
+                a = a))
 }
 
 #calcul de Pgpd
 PGPD<-function(x0,zexc,y,seuil,estim){
   M<-nb_exc(x0,y)
+  result<-FGPD(x0-seuil,zexc,estim)
   if(M >= 10)
-    return(M/length(y))
-  else
-    return(length(zexc)/length(y)*(1-FGPD(x0-seuil,zexc,estim)))
+    return(list(p = M/length(y),
+                k = result$k,
+                a = result$a ))
+  else {
+    return(list( p = length(zexc)/length(y)*(1-result$val),
+                 k = result$k,
+                 a = result$a ))
+  }
 }
 ##Box-Cox transformation ----
 
@@ -77,7 +87,7 @@ BoxCox_lm <- function(Y,X) {  #estimation de lambda par les moindres carres
       X <- log(X)
     else
       X <- (X**lambda - 1)/lambda
-    
+
     b <-  cov(X,Y) / var(X)
     a <- mean(Y) - b*mean(X)
     sum( (Y - a - b*X)**2 )
@@ -85,49 +95,49 @@ BoxCox_lm <- function(Y,X) {  #estimation de lambda par les moindres carres
   optimize(f,c(0,1))$minimum
 }
 
-PBC_Z<-function(z,Ntot,N,param,Zobs){  
+PBC_Z<-function(z,Ntot,N,param,Zobs){
   p<-seq(N,1)/Ntot
   if (missing(param))
     lambda <- BoxCox_lm(log(-log(p)),log(z))
   else
     lambda <- param
   coeffs <- lm(  log(-log(p)) ~ BoxCox(log(z),lambda) )$coefficients
-  return(list(p = exp(-exp(sum( coeffs*c(1, BoxCox(log(Zobs),lambda))))), 
-              interc = coeffs[[1]], 
+  return(list(p = exp(-exp(sum( coeffs*c(1, BoxCox(log(Zobs),lambda))))),
+              interc = coeffs[[1]],
               pente = coeffs[[2]],
               lbda = lambda))
-  
+
 }
 
 ## regroupement des p valeurs des differentes methodes ----
 
-calcul_p<-function(zsim,Ntail=500,estim=c("PWM","EMV"),Zobs,param,method = c("BC","GPD")){  
+calcul_p<-function(zsim,Ntail=500,estim=c("PWM","EMV"),Zobs,param,method = c("BC","GPD")){
   # les Ntail plus grandes valeurs (les dernieres)
-  z1 <- tail( sort(zsim) , Ntail )
+  z1 <- tail( sort(zsim) , Ntail + 1 )
   #seuil pour la GDP
-  t<-(z1[1] + head(sort(zsim,decreasing =TRUE),Ntail+1)[Ntail+1])/2
+  t<-(z1[1] + z1[2])/2
   #calcul des excedents de la GDP, ceux qui lui sont superieurs
+  z1<-z1[-1]
   zgpd<-z1-t
   zgpd<-zgpd[zgpd>0] #uniquement ceux superieurs au seuil
-  
+
   method<-match.arg(method)
-  if (method == "BC")
-    return(list(Pbc_z = PBC_Z(z1,length(zsim),Ntail,param,Zobs)$p,
-                lbda = PBC_Z(z1,length(zsim),Ntail,param,Zobs)$lbda))
-  
+  if (method == "BC") {
+    result<-PBC_Z(z1,length(zsim),Ntail,param,Zobs)
+    return(list(Pbc_z = result$p,
+                lbda = result$lbda))
+  }
+
   if (method =="GPD"){
     estim<-match.arg(estim)
-    if (estim == "PWM")
-      return(list(Pgpd=PGPD(Zobs,zgpd,zsim,t,estim),
-                  a=PWM(zgpd)$a,
-                  k=PWM(zgpd)$k))
-    else
-      return(list(Pgpd=PGPD(Zobs,zgpd,zsim,t,estim),
-                  a=EMV(zgpd)$a,
-                  k=EMV(zgpd)$k))
+    result<-PGPD(Zobs,zgpd,zsim,t,estim)
+    return(list(Pgpd=result$p,
+                  a=result$a,
+                  k=result$k))
+
   }
-  
-    
+
+
 }
 
 

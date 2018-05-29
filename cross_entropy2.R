12763cfc4a563fa98403001f4e633366bff6f361
mariondechallens <35600270+mariondechallens@users.noreply.github.com>
2018-05-29 16:26:53 +0200

    Add files via upload

--- cross_entropy2.R
+++ cross_entropy2.R
@@ -10,7 +10,7 @@ data<-list(x=x, group = x@ped$population, genomic.region = x@snps$gene)
 teststat<-function (data.input) #liste des para de Sum.Fst (x, group, region)
 {
   FST<-Sum.Fst(data.input$x, group = data.input$group, genomic.region = data.input$genomic.region,B.max=0)
-  return(FST$statistic)
+  return(FST$statistic) 
 }
 
 permut<- function (data.input, prop.change) 
@@ -19,12 +19,14 @@ permut<- function (data.input, prop.change)
   m<-floor(n*prop.change)
   I<-sample(1:n,m)
   data.input$group[I]<-sample(data.input$group[I])
+  return(data.input)
   
 }
 
 
 res<-SAMC.adapt(data.input=data,
            t.obs=teststat(data),
+           t.start=rep(0,length(teststat(data))),
            fun.test.statistic=teststat, 
            fun.proposal=permut)
 

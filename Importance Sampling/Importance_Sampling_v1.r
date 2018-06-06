5cee4a7ed6e95f9453512fb27292e54a2d504328
mariondechallens <35600270+mariondechallens@users.noreply.github.com>
2018-06-06 14:38:55 +0200

    Add files via upload

---
+++ Importance Sampling/Importance_Sampling_v1.r
@@ -0,0 +1,37 @@
+Sys.setenv("PKG_CXXFLAGS"="-std=c++11")
+Rcpp::sourceCpp("Importance_Sampling_v1.cpp")
+
+result<-data.frame(matrix(nrow=10,ncol=3))
+colnames(result)<-c("True.p","IS","IS.t.test")
+# l'exemple de EXPERT
+set.seed(1)
+x <- rnorm(200, mean=0, sd=1)
+y <- rnorm(200, mean=0.5, sd=1)
+z <- c(x,y)
+L0 <- c(rep(1L,200),rep(2L,200))
+
+# la matrice de distances entre tous les elts de z
+DIST <- outer(z, z, function(a,b) abs(a-b) )
+
+# exemples
+par(mfrow=c(1,4))
+boxplot(z ~ L0)
+L <- L0; L[1] <- L[1] + 0L # force copie
+k <- c(length(x), length(y))
+IS(k, DIST, L, 0); boxplot(z ~ L)
+IS(k, DIST, L, 1); boxplot(z ~ L)
+IS(k, DIST, L, 3); boxplot(z ~ L)
+
+# F <- function(z, L) abs(t.test(z ~ L, var.equal = TRUE)$statistic)
+F <- function(z, L) abs(t_test(z, L))
+
+t.obs <- F(z, L0);
+# True p-value
+result[1,1]<- 2.0*(1-pt(t.obs, df=length(z)- 2))
+
+
+result[1,2]<-Run_IS(F, z, L0, DIST, 1e5, 0.8, 2)$p
+result[1,3]<-Run_IS_t_test(z, L0, DIST, 1e5, 0.8, 2)$p
+
+# Tr <- replicate(1e4, { IS(k, DIST, L, 0.5); t_test(z, L) } )
+# hist(Tr)

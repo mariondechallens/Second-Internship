f9a4620e65e18bda27719d027cff87e1c510c47f
mariondechallens <35600270+mariondechallens@users.noreply.github.com>
2018-06-20 13:48:32 +0200

    Add files via upload

---
+++ Qt/test/Fenetre principale/mafenetre.h
@@ -0,0 +1,20 @@
+#ifndef MAFENETRE_H
+#define MAFENETRE_H
+
+#include <QtWidgets>
+
+class MaFenetre : public QMainWindow // On hérite de QWidget (IMPORTANT)
+{
+    Q_OBJECT //macro
+
+    public:
+    MaFenetre();
+
+    public slots:
+
+
+    private:
+
+};
+
+#endif // MAFENETRE_H

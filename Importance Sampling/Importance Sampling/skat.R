33e22ad42e4594de6728c671d644a9b46979f3bd
mariondechallens <35600270+mariondechallens@users.noreply.github.com>
2018-06-20 16:13:58 +0200

    Add files via upload

--- Importance Sampling/Importance Sampling/skat.R
+++ Importance Sampling/Importance Sampling/skat.R
@@ -97,12 +97,12 @@ k2dM<-function (Mat, direction = "k2d", method = "norm", scale = 1,
   }
 }
 
-#####"construire le noyau K  =  ZWWZ'
+#####"construire le noyau K  =  ZWwZ'
 
 w<-Get_Logistic_Weights(SKAT.example$Z, par1=0.07, par2=150)
 W<-diag(w)
 K<-SKAT.example$Z%*%W%*%W%*%t(SKAT.example$Z)
-DIST<-k2dM(K,direction = "k2d",method="exp",scale = 2) #theta = 0.5
+DIST<-k2dM(K,direction = "k2d",method="exp",scale = 2) #theta = 1/scale
 
 ######simuler Y sous Ho et estimer fonction de repartition de QSKAT
 ####en binaire
@@ -139,10 +139,14 @@ for (j in 1:length(tobs))
 
 plot(tobs,log10(pvalueskatB),col="blue",type="l")
 
-#####loi normale (0,sigma?In)
+#####loi normale (0,sigma2*In)
 obj2<-SKAT_Null_Model(SKAT.example$y.c ~ 1, out_type="C")
 pvalueskatC<-rep(0,length(tobs))
+pgaston<-rep(0,length(tobs))
+
 #package -> combili de Chi2
+lambda <- eigen(DIST)$values
+
 skat2<-SKAT(SKAT.example$Z,obj2,weights=w)$p.value
 
 
@@ -167,15 +171,35 @@ w <- w / sum(w)
 for (j in 1:length(tobs))
 {
   pvalueskatC[j]<-sum( w*(t.perm  > tobs[j]) )
+  pgaston[j]<-gaston:::davies(tobs[j], lambda, h = rep(1, length(lambda)),
+                     delta = rep(0, length(lambda)), sigma = 0, lim = 10000,
+                     acc = 1e-04) 
 }
 
 plot(tobs,log10(pvalueskatB),col="blue",type="l")
 lines(tobs,log10(pvalueskatC),col="black",type='l')
-legend("topright",legend = c("Y binaire", "Y continue"), fill = c("blue","black"))
+lines(tobs,log10(pgaston),col="forestgreen",type="l")
+legend("topright",legend = c("Y binaire IS", "Y continue IS","Y continue gaston"), fill = c("blue","black","forestgreen"))
 
 plot(tobs,pvalueskatB,col="blue",type="l")
 lines(tobs,pvalueskatC,col="black",type='l')
-legend("topright",legend = c("Y binaire", "Y continue"), fill = c("blue","black"))
+lines(tobs,pgaston,col="forestgreen",type="l")
+legend("topright",legend = c("Y binaire IS", "Y continue IS","Y continue gaston"), fill = c("blue","black","forestgreen"))
 
-print(paste0("Pvalue SKAt binaire = ", skat1))
-print(paste0("Pvalue SKAt continu = ", skat2))
+
+print(paste0("Pvalue SKAt binaire = ",1-skat1)) #p(z>tobs) ?
+print(paste0("Pvalue SKAt continu = ", 1-skat2))
+
+pvalL<-SKAT:::SKAT_Optimal_Linear(res = obj2$res,Z = SKAT.example$Z,
+                                  X1 = obj2$X1,kernel=K,weights = w,
+                                  s2 = obj2$s2, method = "davies", 
+                                  res.out = obj2$res.out,n.Resampling = obj2$n.Resampling,  r.all = 0)$p.value
+pvalB<-SKAT:::SKAT_Optimal_Logistic(res = obj1$res,Z = SKAT.example$Z,
+                                    X1 = obj1$X1,kernel=K,weights = w,
+                                    pi_1 = obj1$pi_1, method = "davies", 
+                                    res.out = obj1$res.out,n.Resampling = obj1$n.Resampling,  r.all = 0)$p.value
+
+                           
+                           
+                           
+                           
